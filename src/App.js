d968109c6cd7bdaad5e8c115a2b18074cf6842e8
Mvisruth <visruthm123@gmail.com>
2024-07-26 17:00:59 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import requests from './request'
 import Row from './components/Row';
 import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav/>
       <Banner fetchUrl={requests.fetchNetflixOriginals}/>
-      <h1 style={{color:"red"}}>Netflix</h1>
+      {/* <h1 style={{color:"red"}}>Netflix</h1> */}
       <Row isPoster={true} title="NetflixOriginals" fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title="Trending Movie" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated Movie" fetchUrl={requests.fetchTopRated}/>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import instance from '../instance'
 
 function Banner({fetchUrl}) {
-    console.log(fetchUrl)
+    const [movie,setMovie]=useState()
+   const  base_url = "https://image.tmdb.org/t/p/original/";
+
+    // console.log(fetchUrl)
+    const fetchData = async()=>{
+     const {data} =   await instance.get(fetchUrl)
+     setMovie(data.results[Math.floor(Math.random()*data.results.length)])
+    } 
+
+    useEffect(()=>{
+       fetchData()  
+    },[])
+    console.log(movie)
   return (
-    <div>Banner</div>
+    <div style={{height:"600px",background:`url(${base_url}${movie?.backdrop_path})`,
+    backgroundPosition:"center",backgroundSize:"cover",backgroundAttachment:"fixed"}}>
+        <div className='banner-content'>
+         <h1>{movie?.name}</h1>
+         <button className='btn btn-danger'>Play<i class="fa-solid fa-play ms-1"></i></button>
+         <button className='btn border-white ms-3'>More info<i class="fa-solid fa-caret-down ms-1"></i></button>
+
+         <h2>{movie?.overview?.slice(0,200)}..</h2>
+        </div>
+    </div>
   )
 }
 
